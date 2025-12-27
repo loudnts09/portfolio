e6192def46704173da3f7ce6b00461914ee8cc09
Lourrayni <lourraynidantass@gmail.com>
2025-12-27 01:38:08 -0300

    fix: remove imports não utilizados para permitir build de produção

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,9 @@
 import Hero from "@/components/sections/Hero";
-import About from "@/components/sections/About";
-import Skills from "@/components/sections/Skills";
-import Projects from "@/components/sections/Projects";
-import Contact from "@/components/sections/Contact";
 
 export default function Home() {
   return (
     <>
       <Hero />
-      {/* <About />
-      <Skills />
-      <Projects />
-      <Contact /> */}
     </>
   );
 }
